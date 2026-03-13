38c864cd81d08dcc17d9740311e7263161368e03
Tobias Böhmerle <tobhmerl@microsoft.com>
2026-03-13 09:57:02 +0100

    Convert to standalone Blazor WASM for GitHub Pages deployment

--- W40k_CheatSheet.Client/Program.cs
+++ W40k_CheatSheet.Client/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using W40k_CheatSheet.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
